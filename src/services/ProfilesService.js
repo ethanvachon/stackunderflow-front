3f780c0033210ef12d5612dcab9ea8ddd69cdf25
ethanvachon <ethanhvachon@gmail.com>
2021-03-30 16:59:08 -0600

    get by profile

--- src/services/ProfilesService.js
+++ src/services/ProfilesService.js
@@ -2,15 +2,15 @@ import { AppState } from '../AppState'
 import { api } from './AxiosService'
 
 class ProfilesService {
-  // async getVaultsByProfile(id) {
-  //   const res = await api.get('api/profiles/' + id + '/vaults')
-  //   AppState.currentVaults = res.data
-  // }
+  async getAnswersByProfile(id) {
+    const res = await api.get('api/profiles/' + id + '/answers')
+    AppState.profileAnswers = res.data
+  }
 
-  // async getKeepsByProfile(id) {
-  //   const res = await api.get('api/profiles/' + id + '/keeps')
-  //   AppState.currentKeeps = res.data
-  // }
+  async getQuestionsByProfile(id) {
+    const res = await api.get('api/profiles/' + id + '/questions')
+    AppState.profileQuestions = res.data
+  }
 
   async getProfile(id) {
     const res = await api.get('api/profiles/' + id)
