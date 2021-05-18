7aec23dfb3e2c684e1be9e428c8fb8bf1155e2fc
ethanvachon <ethanhvachon@gmail.com>
2021-05-17 20:43:30 -0600

    general styling

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,23 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
+::-webkit-scrollbar {
+  width: 10px;
+}
 
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f2f2f2
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgb(177, 177, 177);
+  border-radius: 5px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(146, 146, 146);
+}
 </style>
